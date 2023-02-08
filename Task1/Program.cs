97332af9a6c59f09f5ddd3afbe8910c7fb7e3e82
Jaha <zlgsbazarbaev@gmail.com>
2023-02-08 16:31:58 +0500

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B. Реализовать функцию возведения в степень
+// самостоятельно!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Extent(int a, int b)
+{
+    int result = 1;
+    for (int count = 1; count <= b; count++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int ReadConsole(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+Console.Clear();
+int a = ReadConsole("Enter number A: ");
+int b = ReadConsole("Enter number B: ");
+int pow = Extent(a, b);
+Console.Write(pow);
