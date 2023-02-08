c050d3a267a6b9b8b3458cd86ac6680ae6816bd1
Jaha <zlgsbazarbaev@gmail.com>
2023-02-08 16:56:57 +0500

    added task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая
+// принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Dellet(string[] args)
+{
+    Console.Write("Enter your number: ");
+    int n = int.Parse(Console.ReadLine()!);
+    int sum = 0;
+    int temp = 0;
+    {
+        while (n > 0)
+        {
+            temp = n % 10;
+            sum = sum + temp;
+            n = n / 10;
+        }
+        Console.WriteLine($"The sum = {sum}");
+    }
+}
+Dellet(args);
